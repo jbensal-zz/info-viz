6d88f7aa7abce3199f629bd6a0eddefa2f5de072
Nick <nickljeffrey@gmail.com>
2014-04-15 20:21:30 -0500

    reading in from file

--- backend/findLocs.py
+++ backend/findLocs.py
@@ -1,12 +1,31 @@
 
 #Read file in
 f = open("../data/welsh-hist.txt", 'r')
-for line in f:
-    #parse out address and frequency
+line = f.readline()
+searches = []
+while len(line)>0:
+    words = line.split()
+    if len(words)>0 and words[0] == "Searched":
+        term = " ".join(words[2:-1])
+        line = f.readline()
+        words = line.split()
+        while len(words)>0:
+            #parse out address and frequency
+            if words[-2] == "times":
+                freq = int(words[-3])
+                addr = words[-6]
+            else:
+                freq = 1
+                addr = words[-2]
+            #find physical location for the address
 
-    #find physical location for the address
+            #save into dict for that location
+            searches.append([term, addr, freq])
 
-    #save into dict for that location
-    pass
-
+            #Onto the next one!
+            line = f.readline()
+            words = line.split()
+            pass
+    line = f.readline()
+print searches #TODO: Right now this is an array of arrays of the form [search term, address, frequency]
 #Create json representing circle objects and print to file that javascript can read in
