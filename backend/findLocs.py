9679d07f6fe19241a578c9b4a8ff722a4954ecdf
Nick <nickljeffrey@gmail.com>
2014-04-15 19:30:51 -0500

    moved data to data folder, stubbed a python file for parsing stuffs

---
+++ backend/findLocs.py
@@ -0,0 +1,12 @@
+
+#Read file in
+f = open("../data/welsh-hist.txt", 'r')
+for line in f:
+    #parse out address and frequency
+
+    #find physical location for the address
+
+    #save into dict for that location
+    pass
+
+#Create json representing circle objects and print to file that javascript can read in
